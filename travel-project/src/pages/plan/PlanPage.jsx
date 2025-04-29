4216f8d4245366b5fbe134ecefaec4d829599f70
Hyebin (Helia) <hyebin218@naver.com>
2025-04-29 11:29:03 +0900

    feat #1: DatePickerModal 구현

--- travel-project/src/pages/plan/PlanPage.jsx
+++ travel-project/src/pages/plan/PlanPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import DatePickerModal from '../../components/DatePickerModal';
 
 const PlanPage = () => {
     return (
         <div>
-            
+            <DatePickerModal />
         </div>
     );
 };
