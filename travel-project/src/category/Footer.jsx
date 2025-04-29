4ab7be664dd6571d28f2eea51f2fd871dc1a21c9
Ssun-shine <hayonini@gmail.com>
2025-04-29 23:13:01 +0900

    feat #19 : 네비게이션바, Footer 구현
    - Navbar 형태 민규님 컴포넌트에서 바뀐 부분
      - [리뷰, 나의 여행]부분만 hover 애니매이션 추가
      - [리뷰, 나의 여행] 부분 굵게, 색깔 변경

---
+++ travel-project/src/category/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+function Footer() {
+    return (
+      <footer className="footer">
+        <hr />
+        <p>tel: 010-0000-0000 &nbsp;&nbsp; address:00도00시00동00로00번지</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
