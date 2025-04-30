a23540a0ce8b04342613936d8ab65fad35d70c90
Hyebin (Helia) <hyebin218@naver.com>
2025-04-30 17:15:30 +0900

    feat #7: 일정 추천 페이지 구현 (#22)
    * feat #1: PlanPage 구현
    
    * add #1: react-date-range, date-fns 라이브러리 설치
    
    * feat #1: DatePickerModal 구현
    
    * feat #1: DateRange 초기 세팅
    
    * feat #1: 구간 선택 기능 구현
    
    * feat #1: 날짜 제한 로직 구현
    
    * feat #1: 각 case에 따른 분기 처리
    - 시작일 선택 시 10일까지만 선택 가능
    - 시작일을 다시 선택하면 구간 선택 해제
    - 시작일 선택 후 다른 날짜를 선택하면 범위 선택
    - 이미 범위 선택이 완료된 상태에서 새로운 날짜를 클릭하면 새롭게 범위 선택
    
    * feat #1: 화면 요소 구현
    
    * design #1: style 적용
    
    * feat #1: modal 구현
    
    * design #1: padding 추가
    
    * style #1: import 위치 변경
    
    * feat #2: 이동수단 선택 화면 구현
    
    * design #2: css 적용
    
    * add #7: dummy data 추가
    
    * feat #7: 일정 추천 페이지 구현
    
    * design #7: style 추가
    
    * fix #7: date-fns 라이브러리 관련 오류 해결
    
    * add #7: 테스트용 json 추가
    
    * feat #7: json 파일에서 데이터를 읽어와 필터링 후 화면에 표시
    - locationID와 일치하는 여행지 이름 읽어와 표시
    -  locationID와 일치하는 여행지에서 일정을 여행 기간 만큼 보여줌
    
    * chore #7: custom hook 구현
    
    * feat #7: 여행 일정 부분 component화
    
    * docs #7: 주석 추가

--- travel-project/src/pages/PlanPage/PlanPage.jsx
+++ travel-project/src/pages/PlanPage/PlanPage.jsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react';
-import DatePickerModal from '../../components/DatePickerModal';
-import './PlanPage.css';
+import React, { useState, useEffect } from "react";
+import DatePickerModal from "../../components/DatePickerModal";
+import { useTravelPlan } from "../../hooks/useTravelPlan";
+import TravelPlanList from "../../components/TravelPlanList";
+import "./PlanPage.css";
 
-const PlanPage = () => {
-    const [isModalOpen, setIsModalOpen] = useState(true);
+export const PlanPage = () => {
+  const locationId = "location_001";
+  const transportType = "대중교통";
+  const travelRange = 3;
 
-    return (
-        <div>
-            {isModalOpen && (
-                <div className="modal-overlay">
-                <div className="modal-wrapper">
-                    <DatePickerModal  />
-                </div>
-                </div>
-            )}
-        </div>
-    );
+  const { planData, locationName } = useTravelPlan(
+    locationId,
+    transportType,
+    travelRange
+  );
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  return (
+    <div className="plan-page-container">
+      <div className="plan-page-header">
+        <div className="plan-title">{locationName}</div>
+        <div className="plan-dates">2025.04.25 ~ 2025.05.01</div>
+      </div>
+
+      <TravelPlanList planData={planData} />
+
+      <div className="footer">
+        <button className="save-button">저장하기</button>
+      </div>
+    </div>
+  );
+
+  //TODO: modal 순서대로 나타나게 수정
+  // <div>
+  //     {isModalOpen && (
+  //         <div className="modal-overlay">
+  //             <div className="modal-wrapper">
+  //                 <DatePickerModal  />
+  //             </div>
+  //         </div>
+  //     )}
+  // </div>
 };
