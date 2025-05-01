112a7dde735f0efb29f8c5d09a48a673b622c249
Ssun-shine <hayonini@gmail.com>
2025-05-01 16:08:10 +0900

    design #19: 네비게이션 바 로고 추가

--- travel-project/src/App.js
+++ travel-project/src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import Navbar from "./category/Navbar";
 import Footer from "./category/Footer";
+import { BrowserRouter } from "react-router-dom"; 
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-      <div className="main-content">
-       
+    <BrowserRouter>
+      <div className="app-container">
+        <Navbar />
+        <div className="main-content">
+          {/* 여기에 Routes 나중에 넣을 수 있음 */}
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
