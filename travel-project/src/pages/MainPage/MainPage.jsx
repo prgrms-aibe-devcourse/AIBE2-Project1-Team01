9d0068db84fc03f26da2a34c7006b6c171d8c0eb
alsshsskssl <42994197+alsshsskssl@users.noreply.github.com>
2025-05-02 21:11:34 +0900

    feat/#12 메인 페이지 제작 (#41)
    * feat #12 : TagSelector 구현
    - MainPage에서 사용할 TagSelector 구현
    
    * fix #12 : TagSelector 태그 다중선택 안되는 버그 수정
    
    * feat/#12-1: 태그별 카드 리스트업 구현
    
    * feat #12: 돋보기 클릭 이벤트, 모달 스타일 적용
    
    * design #12 : TagSelector 피그마 디자인에 맞게 수정
    
    * design #12 : search-bar 가로 길이 수정
    
    * feat #12 : RegionCard 상세설명 길이 제한
    
    * fix #12-3 : json 파일 사용하여 카드리스트 수정, Gongju.npg -> Gongju.jpg,  jongno1.jpg 추가, Nonsan.npg -> Nonsan.jpg, sokcho1.jpg 추가,  yongsan.jpg -> yonsan1.jpg ,  3.json : location_01->location_001형식 , 4.json : npg->jpg, 6.json : 형식에 맞게
    
    * feat #12 : RegionCard 클릭 시 TravelDetailPage.jsx의 handleCardClick 함수 호출하도록 수정
    
    * fix #12 : description 길이제한 해제
    
    * feat #12 : TravelDetailPage에 몇번 json파일인지 전달
    
    * feat #12 : originalId TravelDetailPage로 전달하도록 수정
    
    * fix #12 : 데이터 출처, 원본 위치 ID 주석처리
    
    ---------
    
    Co-authored-by: Ssun-shine <hayonini@gmail.com>

--- travel-project/src/pages/MainPage/MainPage.jsx
+++ travel-project/src/pages/MainPage/MainPage.jsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { RegionCard } from "../../components/RegionCard";
+import RegionCardList from "../../components/RegionCardList";
 import "./MainPage.css";
 
 export const MainPage = () => {
   // 이전 태그를 기억하기 위한 레퍼런스
   return (
     <div className="main-page">
-      <div className="main-cardlist">
-        <RegionCard
-          regionName="서울"
-          regionDescription="대한민국의 수도이자 최대 도시입니다."
-          imagePath={require("../../assets/img/test.jpg")}
-          tags={["관광", "맛집", "역사", "도시"]}
-          url="/region/seoul" // 클릭 시 이동할 URL
-        />
-        <RegionCard
-          regionName="서울"
-          regionDescription="대한민국의 수도이자 최대 도시입니다."
-          imagePath={require("../../assets/img/test.jpg")}
-          tags={["관광", "맛집", "역사", "도시"]}
-          url="/region/seoul2" // 클릭 시 이동할 URL
-        />
-      </div>
+      <RegionCardList />
     </div>
   );
 };
